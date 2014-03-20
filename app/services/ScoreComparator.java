2cf3893a98e5da06134176b497a40b5ce34161d6
Thomas McNally <tk.mcnally@gmail.com>
2014-03-20 03:36:33 -0400

    naming

--- app/services/ScoreComparator.java
+++ app/services/ScoreComparator.java
@@ -1,12 +1,13 @@
 package services;
 
 import java.util.Comparator;
+import java.lang.Integer;
 
 import models.User;
 
 public class ScoreComparator implements Comparator<User> {
     @Override
     public int compare(User a, User b) {
-        return java.lang.Integer.compare(java.lang.Integer.parseInt(a.getScore()), java.lang.Integer.parseInt(a.getScore()));
+        return Integer.compare(Integer.parseInt(a.getScore()), Integer.parseInt(a.getScore()));
     }
 }
