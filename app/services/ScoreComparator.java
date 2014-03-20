261dd10fcf4d7804716a96e653d51ae4dca6bd7a
Thomas McNally <tk.mcnally@gmail.com>
2014-03-20 03:41:34 -0400

    naming

--- app/services/ScoreComparator.java
+++ app/services/ScoreComparator.java
@@ -8,7 +8,14 @@ import models.User;
 public class ScoreComparator implements Comparator<User> {
     @Override
     public int compare(User a, User b) {
-    	int return_num = Integer.compare(Integer.parseInt(a.getScore()), Integer.parseInt(a.getScore()));
-        return return_num;
+    	Integer id1 = Integer.parseInt(a.getScore());
+	    Integer id2 = Integer.parseInt(a.getScore());
+	    if (id1 == null) {
+	        return id2 == null ? 0 : 1;
+	    }
+	    if (id2 == null) {
+	        return -1;
+	    }
+	    return id1.compareTo(id2);
     }
 }
