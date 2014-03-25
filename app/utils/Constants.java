35264d64bfe42d54de1ed808d0ded32dfbfa3235
Thomas McNally <tk.mcnally@gmail.com>
2014-03-25 01:18:27 -0400

    Added scoring logic

--- app/utils/Constants.java
+++ app/utils/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
 
 	public static String DB_NAME = "app20699811";
 	
-	public static String DEFAULT_QUESTION = "Please select a question!";
+	public static String DEFAULT_QUESTION = "Select a question!";
 }
